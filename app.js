b5b756ccee24183eb9885c96d43281254112cb3e
Ismail Ali <ismailchangezi801@gmail.com>
2022-09-13 02:24:27 +0500

    Update App.js

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const menu = document.querySelector('.menu');
-// const menuItems = document.querySelectorAll(".menuItem");
-const hamburger = document.querySelector('.hamburger');
-const closeIcon = document.querySelector('.closeIcon');
-const menuIcon = document.querySelector('.menuIcon');
-
-closeIcon.style.display = 'none';
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+closeIcon.style.display = "none";
 function toggleMenu() {
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu');
-    closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   } else {
-    menu.classList.add('showMenu');
-    closeIcon.style.display = 'flex';
-    menuIcon.style.display = 'none';
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "flex";
+    menuIcon.style.display = "none";
   }
 }
-
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
