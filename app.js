fc0c9bc303ed89d4da64fe5eb508f14bf4699c53
Ismail Ali <ismailchangezi801@gmail.com>
2022-09-13 23:16:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -15,3 +15,17 @@ function toggleMenu() {
   }
 }
 hamburger.addEventListener("click", toggleMenu);
+
+// Popup Window
+
+const open = document.getElementById("open");
+const modal = document.getElementById("modal-container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
